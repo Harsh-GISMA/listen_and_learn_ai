df8dc6f49045b4a01753b61f769f51781667b9f0
Harsh Deshmukh <harsh.uniadmission24@gmail.com>
2025-12-18 16:09:41 +0000

    As I am having problem installing NLTK package in streamlit cloud I have changed the nlp_keywords file in order to solve this problem.

--- nlp_keywords.py
+++ nlp_keywords.py
@@ -1,20 +1,18 @@
 from collections import Counter
 import re
 
-import nltk
-from nltk.corpus import stopwords
-
-STOPWORDS = set(stopwords.words("english"))
-
+STOPWORDS = {
+    "the", "and", "a", "an", "to", "of", "in", "on", "for", "is", "are",
+    "was", "were", "it", "this", "that", "with", "as", "by", "at", "from",
+    "or", "be", "we", "you", "they", "i", "our", "their", "your",
+    "so", "if", "but", "not", "no", "do", "does", "did", "can", "could",
+    "should", "would", "have", "has", "had", "will", "just", "about",
+    "into", "out", "up", "down", "over", "under", "than", "then", "very"
+}
 
 def extract_keywords(text: str, top_n: int = 10):
     text = text.lower()
-    # keep words only
     tokens = re.findall(r"[a-zA-Z]+", text)
-
-    # filter stopwords and tiny words
     tokens = [t for t in tokens if t not in STOPWORDS and len(t) > 2]
-
     counts = Counter(tokens)
-    keywords = [w for w, _ in counts.most_common(top_n)]
-    return keywords
+    return [w for w, _ in counts.most_common(top_n)]
